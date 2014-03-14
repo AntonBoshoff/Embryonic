7c9ba20ad6ccad35185f9e0150d2071802db6972
Anton Boshoff <anton@digitalpro.co.za>
2014-03-14 12:51:03 +0200

    initial project commit

---
+++ dev-assets/js/script.js
@@ -0,0 +1,28 @@
+// EMBRYONIC FRAMEWORK : Anton Boshoff //
+
+
+// === INDEX === //
+
+
+// === NAMESPACE === //
+window.app = {}
+
+
+// === TRIGGER ON DOCUMENT READY === //
+$(function(){
+
+	try {
+
+	} catch(err) {
+
+	}
+
+});
+
+
+// === FUNCTIONS === //
+app.go = {
+
+	
+
+}
